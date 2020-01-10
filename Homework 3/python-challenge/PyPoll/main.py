8fcd0081b4e771fb9197a1e8fd79209017b1262d
Chris <christopherchiang92@gmail.com>
2020-01-10 15:59:38 -0800

    pep8 edits

--- Homework 3/python-challenge/PyPoll/main.py
+++ Homework 3/python-challenge/PyPoll/main.py
@@ -1,16 +1,16 @@
-#Using code from PyBank's main.py as starter
+# Using code from PyBank's main.py as starter
 
-#import libraries and setting csv data path
+# import libraries and setting csv data path
 import os
 import csv
-bank_csv = os.path.join('.', 'Resources', 'election_data.csv')
+poll_csv = os.path.join('.', 'Resources', 'election_data.csv')
 
 # Using code from class activities to read csv
-with open(bank_csv, 'r') as csvfile:
+with open(poll_csv, 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
 
     # Initialize vars
-    # Using a dictionary to store candidates as a name and vote total pair
+    # Using a dictionary to store candidates as a name and vote pair
     total_votes = 0
     candidates = {}
     # skipping header using next()
@@ -19,7 +19,7 @@ with open(bank_csv, 'r') as csvfile:
     for row in csvreader:
         total_votes += 1
         candidate_name = row[2]
-        #if candidate name is already in add one vote else initialize
+        # if candidate name is already in add one vote else initialize
         if candidate_name in candidates:
             candidates[candidate_name] += 1
         else:
@@ -31,25 +31,24 @@ with open(bank_csv, 'r') as csvfile:
     with open(outpath, 'w') as out_text:
         print("Election Results")
         out_text.write("Election Results \n")
-        print("-"*50)
-        out_text.write("-"*50+"\n")
+        print("-" * 50)
+        out_text.write("-" * 50 + "\n")
         print(f"Total Votes: {total_votes}")
         out_text.write(f"Total Votes: {total_votes} \n")
-        #init var for comparing winner
-        winner=''
-        winner_votes=0
-        #for each name in candidates print results and check who has most votes
+        # init var for comparing winner
+        winner = ''
+        winner_votes = 0
+        # for each name in candidates print results and check who has most
+        # votes
         for cand in candidates:
-            votes=candidates[cand]
-            vote_percent = round(votes/total_votes*100, 6)
-            if votes>winner_votes:
-                winner_votes=votes
-                winner=cand
+            votes = candidates[cand]
+            vote_percent = round(votes / total_votes * 100, 6)
+            if votes > winner_votes:
+                winner_votes = votes
+                winner = cand
             print(f"{cand}: {vote_percent}%, Votes: {votes}")
             out_text.write(f"{cand}: {vote_percent}%, Votes: {votes} \n")
-        print("-"*50)
-        out_text.write("-"*50+"\n")
+        print("-" * 50)
+        out_text.write("-" * 50 + "\n")
         print(f"Winner: {winner}")
         out_text.write(f"Winner: {winner}")
-
-    
