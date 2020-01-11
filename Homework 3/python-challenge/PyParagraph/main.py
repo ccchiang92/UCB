f99bf185cfc708f80c361d291207743a04612ab8
Chris <christopherchiang92@gmail.com>
2020-01-11 12:05:38 -0800

    pep8 edits

--- Homework 3/python-challenge/PyParagraph/main.py
+++ Homework 3/python-challenge/PyParagraph/main.py
@@ -1,29 +1,29 @@
-#Import regex and os
+# Import regex and os
 import os
 import re
 # Set file to read, input dfferent number to read different files
-filenumber=input("Please Enter File Number ")
-file_path=os.path.join(".","raw_data",f"paragraph_{filenumber}.txt")
+filenumber = input("Please Enter File Number ")
+file_path = os.path.join(".", "raw_data", f"paragraph_{filenumber}.txt")
 
-with open(file_path,"r",encoding="utf-8") as text:
-    #Read file into a string
-    paragraph=text.read()
-    #Create a list of words
-    words=paragraph.split()
+with open(file_path, "r", encoding="utf-8") as text:
+    # Read file into a string
+    paragraph = text.read()
+    # Create a list of words
+    words = paragraph.split()
 
-    #Splits paragraph in to a list of lines, where each line is a string
-    sen_linesplit=re.split(r'[\r\n]+',paragraph)
-    sentances=[]
+    # Splits paragraph in to a list of lines, where each line is a string
+    sen_linesplit = re.split(r'[\r\n]+', paragraph)
+    sentances = []
 
     # For each line split text into sentences that ends with .!?
     for sentance in sen_linesplit:
-        sentanceOfEachLine=re.split("(?<=[.!?]) +", sentance)
+        sentanceOfEachLine = re.split("(?<=[.!?]) +", sentance)
         # Add each sentence into one counting list
         for sen in sentanceOfEachLine:
             sentances.append(sen)
 
     # Count total number of words
-    letter_count=0
+    letter_count = 0
     for word in words:
         letter_count += len(word)
     # Print results using len of lists generated
@@ -33,4 +33,3 @@ with open(file_path,"r",encoding="utf-8") as text:
     print(f"Approximate Sentence Count: {len(sentances)}")
     print(f"Average Letter Count: {round(letter_count/len(words),4)}")
     print(f"Average Sentence Length: {round(len(words)/len(sentances),4)}")
-    
